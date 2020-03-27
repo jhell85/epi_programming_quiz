cd7a10850c2109b73806f3b065a4c1d157624c03
josh <jhell85@gmail.com>
2020-03-27 16:12:27 -0700

    added functions for buttons on q4 slide

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,57 @@
 $(document).ready(function() {
-$("#start").click(function(){
-  $("#start-container").addClass("hidden");
-  $("#q1").removeClass("hidden")
-})
-$("#color1").click(function(){
-  changeColor("c1")
-})
-$("#color2").click(function(){
-  changeColor("c2")
-})
-$("#color3").click(function(){
-  changeColor("c3")
-})
-$("#color4").click(function(){
-  changeColor("c4")
-})
-
+  let frontEnd;
+  let spiritAnimal;
+  $("#start").click(function() {
+    $("#start-container").addClass("hidden");
+    $("#q1").removeClass("hidden");
+  });
+  $("#color1").click(function() {
+    changeColor("c1");
+  });
+  $("#color2").click(function() {
+    changeColor("c2");
+  });
+  $("#color3").click(function() {
+    changeColor("c3");
+  });
+  $("#color4").click(function() {
+    changeColor("c4");
+  });
+  $("#q1next").click(function() {
+    $("#q1").addClass("hidden");
+    $("#q2").removeClass("hidden");
+  });
+  $("#frontend").click(function(){
+    $("#q2").addClass("hidden");
+    $("#q3").removeClass("hidden");
+    frontEnd = true
+  })
+  $("#backend").click(function(){
+    $("#q2").addClass("hidden");
+    $("#q3").removeClass("hidden");
+    frontEnd = false
+  })
+  $("#spirit1").click(function(){
+    q3complete(1)
+  })
+  $("#spirit2").click(function(){
+    q3complete(2)
+  })
+  $("#spirit3").click(function(){
+    q3complete(3)
+  })
+  $("#spirit4").click(function(){
+    q3complete(4)
+  })
 });
-let changeColor = (function(colors){
+
+let changeColor = function(colors) {
   $("body").removeClass();
   $("body").addClass(colors);
-})
+};
+
+let q3complete = function(spiritNumber) {
+  $("#q3").addClass("hidden");
+  $("#q4").removeClass("hidden");
+  spiritAnimal = spiritNumber
+}
