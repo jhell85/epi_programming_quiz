29112b0d013f3eb016c9e970e6ba365b5097aae2
josh <jhell85@gmail.com>
2020-03-27 13:01:54 -0700

    added functionality to color buttons

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,21 @@ $("#start").click(function(){
   $("#start-container").addClass("hidden");
   $("#q1").removeClass("hidden")
 })
+$("#color1").click(function(){
+  changeColor("c1")
+})
+$("#color2").click(function(){
+  changeColor("c2")
+})
+$("#color3").click(function(){
+  changeColor("c3")
+})
+$("#color4").click(function(){
+  changeColor("c4")
+})
 
 });
+let changeColor = (function(colors){
+  $("body").removeClass();
+  $("body").addClass(colors);
+})
