107e0e9529d998317204cb336486cf756cb13884
josh <jhell85@gmail.com>
2020-03-28 13:48:23 -0700

    added functions for q4 and q5 forms

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   let frontEnd;
   let spiritAnimal;
+  let question4;
+  let mathAnswer;
   $("#start").click(function() {
     $("#start-container").addClass("hidden");
     $("#q1").removeClass("hidden");
@@ -21,28 +23,53 @@ $(document).ready(function() {
     $("#q1").addClass("hidden");
     $("#q2").removeClass("hidden");
   });
-  $("#frontend").click(function(){
+  $("#frontend").click(function() {
     $("#q2").addClass("hidden");
     $("#q3").removeClass("hidden");
-    frontEnd = true
-  })
-  $("#backend").click(function(){
+    frontEnd = true;
+  });
+  $("#backend").click(function() {
     $("#q2").addClass("hidden");
     $("#q3").removeClass("hidden");
-    frontEnd = false
-  })
-  $("#spirit1").click(function(){
-    q3complete(1)
-  })
-  $("#spirit2").click(function(){
-    q3complete(2)
-  })
-  $("#spirit3").click(function(){
-    q3complete(3)
-  })
-  $("#spirit4").click(function(){
-    q3complete(4)
-  })
+    frontEnd = false;
+  });
+  $("#spirit1").click(function() {
+    q3complete();
+    spiritAnimal = 1;
+  });
+  $("#spirit2").click(function() {
+    q3complete(2);
+    spiritAnimal = 2;
+  });
+  $("#spirit3").click(function() {
+    q3complete(3);
+    spiritAnimal = 3;
+  });
+  $("#spirit4").click(function() {
+    q3complete(4);
+    spiritAnimal = 4;
+  });
+  $("#q4o1").click(function() {
+    mathAnswer = q4complete();
+    question4 = 3;
+    console.log(
+      `frontEnd: ${frontEnd} spiritAnimal: ${spiritAnimal} question4:${question4} mathAnswer: ${mathAnswer} `
+    );
+  });
+  $("#q4o2").click(function() {
+    mathAnswer = q4complete();
+    question4 = 2;
+    console.log(
+      `frontEnd: ${frontEnd} spiritAnimal: ${spiritAnimal} question4:${question4} mathAnswer: ${mathAnswer} `
+    );
+  });
+  $("#q4o3").click(function() {
+    mathAnswer = q4complete();
+    question4 = 1;
+    console.log(
+      `frontEnd: ${frontEnd} spiritAnimal: ${spiritAnimal} question4:${question4} mathAnswer: ${mathAnswer} `
+    );
+  });
 });
 
 let changeColor = function(colors) {
@@ -50,8 +77,22 @@ let changeColor = function(colors) {
   $("body").addClass(colors);
 };
 
-let q3complete = function(spiritNumber) {
+let q3complete = function() {
   $("#q3").addClass("hidden");
   $("#q4").removeClass("hidden");
-  spiritAnimal = spiritNumber
-}
+};
+
+let q4complete = function() {
+  $("#q4").addClass("hidden");
+  $("#q5").removeClass("hidden");
+  mathAnswer = getRandomInts();
+  return mathAnswer;
+};
+
+let getRandomInts = function() {
+  let int1 = Math.floor(Math.random() * 10);
+  let int2 = Math.floor(Math.random() * 10);
+  $("#firstInt").append(`   ${int1}`);
+  $("#secondInt").append(`+ ${int2}`);
+  return int1 + int2;
+};
